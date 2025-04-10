696acadb5b6b4f71626abedd34667a9da57f7bd9
marinakochuten <marinakochuten@gmail.com>
2025-04-10 12:40:15 -0700

    Outline workflow for function

--- almond-yield.R
+++ almond-yield.R
@@ -1,1 +1,13 @@
 # Insert function code 
+calculate_almond_yield <- function() {
+  
+  # Aggregate daily climate data to monthly averages ----
+
+  
+  # Calculate annual yield ----
+  
+  
+  # Extract minimum, maximum, and average yields ----
+  
+  
+}
